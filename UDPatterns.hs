2453cfb1474091d7e757e0f0daebd3794fbe29ec
Arianna Masciolini <uzkamascio@gmail.com>
2024-04-17 18:28:50 +0200

    more docs/cleanup for UDPatterns but not done

--- UDPatterns.hs
+++ UDPatterns.hs
@@ -7,7 +7,8 @@ Stability   : experimental
 Portability : POSIX
 
 Embedded DSL and functions for pattern matching and replacement on 'UDTree's.
-For additional information about the semantics of UD patterns, see
+For additional information about the semantics of UD patterns, see the 
+[pattern matching and replacement docs](https://github.com/harisont/deptreehs/blob/main/pattern_matching_and_replacement.md).
 -}
 
 {-# LANGUAGE DeriveDataTypeable #-}
@@ -23,7 +24,11 @@ import UDStandard
 import RTree
 import UDTrees
 
--- | Constructor for pattern matching
+-- * Pattern matching
+
+-- $
+-- Pattern matching DSL
+
 data UDPattern =
     FORM String
   | LEMMA String
@@ -52,73 +57,61 @@ data UDPattern =
   | LENGTH_OVER Int
  deriving (Show,Read,Eq,Typeable,Data)
 
-showMatchesInUDSentence :: UDPattern -> UDSentence -> String
-showMatchesInUDSentence p s =
-  if   null matches then ""
-  else unlines (udCommentLines s ++ matches)
- where
-   matches = [prt (adjust t) | t <- matchesUDPattern p (sentence2tree s)]
-   adjust
-     | otherwise = tree2sentence
-
-matchesUDPattern :: UDPattern -> UDTree -> [UDTree]
-matchesUDPattern p tree@(RTree node subtrees) = case p of
+-- | Given a pattern and a tree, return a list of subtrees matching the 
+-- pattern
+matchingSubtrees :: UDPattern -> UDTree -> [UDTree]
+matchingSubtrees p tree@(RTree node subtrees) = case p of
   SEQUENCE ps  -> maybe [] return $ findMatchingUDSequence True ps tree
   SEQUENCE_ ps  -> maybe [] return $ findMatchingUDSequence False ps tree
-  _ -> [tree | ifMatchUDPattern p tree] ++ concatMap (matchesUDPattern p) subtrees
+  _ -> [tree | matchesPattern p tree] 
+    ++ concatMap (matchingSubtrees p) subtrees
 
-showReplacementsInUDSentence :: UDReplacement -> UDSentence -> String
-showReplacementsInUDSentence rep s =
-  prt (ns{
-    udCommentLines = udCommentLines s ++
-    if changed then ["# newtext = " ++ unwords (map udFORM (udWordLines ns))] else []
-    })
- where
-   ns = tree2sentence $ subtree2tree tr
-   (tr,changed) = replacementsWithUDPattern rep (sentence2tree s)
-
-replacementsWithUDPattern :: UDReplacement -> UDTree -> (UDTree,Bool)
-replacementsWithUDPattern rep tree = case replaceWithUDPattern rep tree of
-  (RTree node subtrs,b) -> let (trs,bs) = unzip (map (replacementsWithUDPattern rep) subtrs)
-                           in (RTree node trs, or (b:bs))
-
-ifMatchUDPattern :: UDPattern -> UDTree -> Bool
-ifMatchUDPattern patt tree@(RTree node subtrees) = case patt of
+-- | Return  'True' if a tree (__or any of its subtrees__) 
+-- matches a certain pattern, 'False' otherwise
+matchesPattern :: UDPattern -> UDTree -> Bool
+matchesPattern patt tree@(RTree node subtrees) = case patt of
   FORM s -> matchString s (udFORM node)
   LEMMA s -> matchString s (udLEMMA node)
   UPOS s -> matchString s (udUPOS node)
   XPOS s -> matchString s (udXPOS node)
-  MISC name s -> maybe False (matchString s) $ listToMaybe [ intercalate "," vals | UDData arg vals <- udMISC node , arg == name  ]
+  MISC name s -> maybe 
+    False 
+    (matchString s) $ listToMaybe 
+      [intercalate "," vals | 
+       UDData arg vals <- udMISC node , arg == name]
   FEATS udds -> udFEATS node == prs udds
   FEATS_ udds -> 
     let uddlist = prs udds in
     or [fs == uddlist | fs <- sublists (length uddlist) (udFEATS node)]
   DEPREL s -> matchString s (udDEPREL node)
   DEPREL_ s -> matchString s (takeWhile (/=':') (udDEPREL node))
-  AND ps -> and [ifMatchUDPattern p tree | p <- ps]
-  OR ps -> or [ifMatchUDPattern p tree | p <- ps]
-  NOT p -> not (ifMatchUDPattern p tree)
-  SEQUENCE ps -> maybe False (const True) $ findMatchingUDSequence True ps tree
-  SEQUENCE_ ps -> maybe False (const True) $ findMatchingUDSequence False ps tree
-  TREE p ps -> ifMatchUDPattern p tree
+  AND ps -> and [matchesPattern p tree | p <- ps]
+  OR ps -> or [matchesPattern p tree | p <- ps]
+  NOT p -> not (matchesPattern p tree)
+  SEQUENCE ps -> 
+    maybe False (const True) $ findMatchingUDSequence True ps tree
+  SEQUENCE_ ps -> 
+    maybe False (const True) $ findMatchingUDSequence False ps tree
+  TREE p ps -> matchesPattern p tree
     && length ps == length subtrees
-    && and [ifMatchUDPattern q t | (q,t) <- zip ps subtrees]
+    && and [matchesPattern q t | (q,t) <- zip ps subtrees]
   TREE_ p ps ->
-    or [ifMatchUDPattern (TREE p ps) (RTree node qs) | qs <- sublists (length ps) subtrees]
+    or [matchesPattern (TREE p ps) (RTree node qs) 
+        | qs <- sublists (length ps) subtrees]
   TRUE -> True
   PROJECTIVE -> isProjective tree
-  ARG pos deprel -> ifMatchUDPattern (AND [UPOS pos, DEPREL deprel]) tree
+  ARG pos deprel -> matchesPattern (AND [UPOS pos, DEPREL deprel]) tree
   DEPTH d -> depth tree == d
   DEPTH_UNDER d -> depth tree < d
   DEPTH_OVER d -> depth tree > d
   LENGTH d -> length (allNodes tree) == d
   LENGTH_UNDER d -> length (allNodes tree) < d
   LENGTH_OVER d -> length (allNodes tree) > d
-
-matchString p s = case p of
-  '*':pp -> pp == drop (length s - length pp) s
-  _:_ | last p =='*' -> init p == take (length (init p)) s
-  _ -> p == s
+  where
+    matchString p s = case p of
+      '*':pp -> pp == drop (length s - length pp) s
+      _:_ | last p =='*' -> init p == take (length (init p)) s
+      _ -> p == s
   
 findMatchingUDSequence :: Bool -> [UDPattern] -> UDTree -> Maybe UDTree
 findMatchingUDSequence strict ps tree 
@@ -127,7 +120,7 @@ findMatchingUDSequence strict ps tree
   | otherwise =   --- makes sense only for node-matching patterns
        case [snodes |
              snodes <- parts (length ps) nodes,
-             all (\ (p,n) -> ifMatchUDPattern p (RTree n [])) (zip ps snodes)
+             all (\ (p,n) -> matchesPattern p (RTree n [])) (zip ps snodes)
              ] of
          snodes:_ -> smallestSpanningUDSubtree (begin snodes) (end snodes) tree
          _ -> Nothing
@@ -137,6 +130,7 @@ findMatchingUDSequence strict ps tree
   begin ns = id2int (udID (head ns)) -- exists because ps > 0
   end ns = id2int (udID (last ns))
 
+-- * Pattern replacement
 
 data UDReplacement =
     REPLACE_FORM String String
@@ -161,36 +155,36 @@ data UDReplacement =
 
 replaceWithUDPattern :: UDReplacement -> UDTree -> (UDTree,Bool)
 replaceWithUDPattern rep tree@(RTree node subtrs) = case rep of
-  REPLACE_FORM old new | ifMatchUDPattern (FORM old) tree -> true $ tree{root = node{udFORM = new}}
-  REPLACE_LEMMA old new | ifMatchUDPattern (LEMMA old) tree -> true $ tree{root = node{udLEMMA = new}}
-  REPLACE_POS old new | ifMatchUDPattern (UPOS old) tree -> true $ tree{root = node{udUPOS = new}}
-  REPLACE_XPOS old new | ifMatchUDPattern (XPOS old) tree -> true $ tree{root = node{udXPOS = new}}
-  REPLACE_MISC name old new | ifMatchUDPattern (MISC name old) tree -> true $ tree{root = node{udMISC = map (\ud -> if udArg ud == name then ud{udVals = getSeps ',' new} else ud) (udMISC node)}}
-  REPLACE_DEPREL old new | ifMatchUDPattern (DEPREL old) tree -> true $ tree{root = node{udDEPREL = new}}
-  REPLACE_DEPREL_ old new | ifMatchUDPattern (DEPREL_ old) tree -> true $ tree{root = node{udDEPREL = new}}
-  REPLACE_FEATS old new | ifMatchUDPattern (FEATS old) tree -> true $ tree{root = node{udFEATS = prs new}}
-  REPLACE_FEATS_ old new | ifMatchUDPattern (FEATS_ old) tree -> true $
+  REPLACE_FORM old new | matchesPattern (FORM old) tree -> true $ tree{root = node{udFORM = new}}
+  REPLACE_LEMMA old new | matchesPattern (LEMMA old) tree -> true $ tree{root = node{udLEMMA = new}}
+  REPLACE_POS old new | matchesPattern (UPOS old) tree -> true $ tree{root = node{udUPOS = new}}
+  REPLACE_XPOS old new | matchesPattern (XPOS old) tree -> true $ tree{root = node{udXPOS = new}}
+  REPLACE_MISC name old new | matchesPattern (MISC name old) tree -> true $ tree{root = node{udMISC = map (\ud -> if udArg ud == name then ud{udVals = getSeps ',' new} else ud) (udMISC node)}}
+  REPLACE_DEPREL old new | matchesPattern (DEPREL old) tree -> true $ tree{root = node{udDEPREL = new}}
+  REPLACE_DEPREL_ old new | matchesPattern (DEPREL_ old) tree -> true $ tree{root = node{udDEPREL = new}}
+  REPLACE_FEATS old new | matchesPattern (FEATS old) tree -> true $ tree{root = node{udFEATS = prs new}}
+  REPLACE_FEATS_ old new | matchesPattern (FEATS_ old) tree -> true $
     let news = [(udArg fv, udVals fv) | fv <- prs new] in
     tree{root = node{udFEATS = [maybe fv (\v -> fv{udVals = v}) (lookup (udArg fv) news) | fv <- udFEATS node]}}
-  IF cond replace | ifMatchUDPattern cond tree -> replaceWithUDPattern replace tree
-  UNDER cond replace | ifMatchUDPattern cond tree -> true $ tree{subtrees = map (fst . replaceWithUDPattern replace) subtrs} 
-  OVER cond replace | any (ifMatchUDPattern cond) subtrs -> replaceWithUDPattern replace tree
-  PRUNE cond depth | ifMatchUDPattern cond tree -> true $ flattenRTree depth tree
-  FILTER_SUBTREES cond scond | ifMatchUDPattern cond tree ->
-    let sts = [st | st <- subtrs, ifMatchUDPattern scond st]
+  IF cond replace | matchesPattern cond tree -> replaceWithUDPattern replace tree
+  UNDER cond replace | matchesPattern cond tree -> true $ tree{subtrees = map (fst . replaceWithUDPattern replace) subtrs} 
+  OVER cond replace | any (matchesPattern cond) subtrs -> replaceWithUDPattern replace tree
+  PRUNE cond depth | matchesPattern cond tree -> true $ flattenRTree depth tree
+  FILTER_SUBTREES cond scond | matchesPattern cond tree ->
+    let sts = [st | st <- subtrs, matchesPattern scond st]
     in (RTree node sts, length sts /= length subtrs)
-  RETARGET cond patt1 patt2 | ifMatchUDPattern cond tree ->
+  RETARGET cond patt1 patt2 | matchesPattern cond tree ->
     let
-      newhead = [subtr | subtr <- subtrs, ifMatchUDPattern patt2 subtr]
+      newhead = [subtr | subtr <- subtrs, matchesPattern patt2 subtr]
       retarget st = case newhead of
         subtr:_ | udID (root st) == udID (root subtr) ->
           [subtr{subtrees = subtrees subtr ++
-              [t{root = (root t){udHEAD = udID (root subtr)}} | t <- subtrs, ifMatchUDPattern patt1 t] }]
-        _:_ | ifMatchUDPattern patt1 st -> []
+              [t{root = (root t){udHEAD = udID (root subtr)}} | t <- subtrs, matchesPattern patt1 t] }]
+        _:_ | matchesPattern patt1 st -> []
         _ -> [st]
       sts = concat [retarget st | st <- subtrs]
     in (RTree node sts, length sts /= length subtrs)
-  FLATTEN cond | ifMatchUDPattern cond tree ->
+  FLATTEN cond | matchesPattern cond tree ->
     let sts = concat
                 [subtr{subtrees = []} :
                   [t{root = (root t){udHEAD = udID node}} | t <- subtrees subtr]
@@ -261,6 +255,12 @@ segments n xs =
   else []
 
 
-
+-- | Given a replacement and a tree, return a list of subtrees matching the 
+-- pattern and a flag indicating whether the tree has been modified or not
+replacementsWithUDPattern :: UDReplacement -> UDTree -> (UDTree,Bool)
+replacementsWithUDPattern rep tree = case replaceWithUDPattern rep tree of
+  (RTree node subtrs,b) -> 
+    let (trs,bs) = unzip (map (replacementsWithUDPattern rep) subtrs)
+    in (RTree node trs, or (b:bs))
 
  
